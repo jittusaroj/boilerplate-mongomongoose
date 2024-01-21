616b08cb3b8792519af3e15b1ef923ee339cbc28
jitendra kumar saroj <saroj222137@gmail.com>
2024-01-21 16:27:35 +0530

    created schema

--- myApp.js
+++ myApp.js
@@ -3,7 +3,19 @@ require('dotenv').config();
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+const personSchema  =  new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number
+  },
+  favoriteFoods: {
+    type: [String]
+  }
+});
+const Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
